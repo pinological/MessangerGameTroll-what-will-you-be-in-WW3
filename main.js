325e14d0f30af31489367529a772683ed520736b
pinological <captainpeter70@gmail.com>
2020-01-12 10:14:44 +0545

    second

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function okboomer() {
+    var name = document.getElementById("name").value;
     var guy = ["Photoguy", "badass army guy", "confuse guy", "bush guy", "cricket guy", "tea lover", "guy with elite pass", "melee weapons guy", "guy who stole lran weapons", "chad"];
     let getran = function(start, range) {
         let getrand = Math.floor((Math.random() * range) + start);
@@ -8,8 +9,9 @@ function okboomer() {
         return getrand;
     }
     console.log("ok boomer");
+    console.log(name);
     console.log(getran(1, 10));
     let numb = getran(1, 10);
-    document.getElementById("maintext").innerHTML = "You will be that " + " " + guy[(numb - 1)];
+    document.getElementById("maintext").innerHTML = name + " " + "you will be that " + " " + guy[(numb - 1)];
     document.getElementById("main").innerHTML = "<img src=" + "/photo/" + numb + ".jpg>";
 }
